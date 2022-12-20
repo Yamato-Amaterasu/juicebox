0a1b09b363affc5ef30e96375fd2681a12e5c456
Mohamad Ayyad <hamoodaayyad@gmail.com>
2022-12-20 14:52:45 -0600

    part 2.5 of part 3

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,34 @@
 const express = require("express");
 const postsRouter = express.Router();
-const { getAllPosts } = require("../db/functions/allFunctions");
+const { getAllPosts, createPost } = require("../db/functions/allFunctions");
+const { requireUser } = require("./utils");
+
+postsRouter.post("/", requireUser, async (req, res, next) => {
+  console.log(req.user);
+  // res.send("hi");
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    postData.content = content;
+    postData.title = title;
+    postData.authorId = req.user.id;
+
+    const post = await createPost(postData);
+    console.log("-----------------------", post, "----------------------");
+    if (post) {
+      res.send(post);
+    } else next(error);
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 postsRouter.use((req, res, next) => {
   console.log("a request was made to /posts");
